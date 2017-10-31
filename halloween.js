d54cda99f2b1808429afc120b75db7df17623b15
David Nyman <davidnyman@Davids-MacBook-Pro.local>
2017-10-31 09:41:39 -0400

    add history

--- halloween.js
+++ halloween.js
@@ -1,2 +1,25 @@
 var candy = ["Reeses", "Snickers", "Milkyway Midnight"]
 var costumes = ["Spider-man", "Ghost", "Skeleton"]
+var candy = ["Reeses", "Snickers", "Milkyway Midnight"]
+var costumes = ["Spider-man", "Ghost", "Skeleton"]
+var history = ["579  mkdir getGit",
+               "580  cd getGit",
+               "581  git init",
+               "582  touch halloween.js",
+               "583  ls",
+               "584  open halloween.js",
+               "585  git status",
+               "586  git add halloween.js",
+               "587  git commit -m "add candy"",
+               "588  git status",
+               "589  git remote add origin https://github.com/davidnymanmusic/halloween-candy.git",
+               "590  git push -u origin master",
+               "591  git status",
+               "592  git add halloween.js",
+               "593  git commit -m "add candy for real this time"",
+               "594  git remote -v",
+               "595  git status",
+               "596  git commit -m "add costumes"",
+               "597  git add halloween.js",
+               "598  git commit -m "add costumes"",
+               "599  git push"]
